dff7db7f2f058becfe2744d12a4b9652ae9b748c
kikito <kikito@gmail.com>
2014-01-20 21:43:35 +0100

    rebuilt to use ngx.shared.DICT + new syntax

--- lib/statsd.lua
+++ lib/statsd.lua
@@ -1,28 +1,58 @@
-local Statsd = {}
+local statsd = {}
 
-Statsd.time  = function (bucket, time) Statsd.register(bucket, time .. "|ms") end
-Statsd.count = function (bucket, n)    Statsd.register(bucket, n .. "|c") end
-Statsd.incr  = function (bucket)       Statsd.count(bucket, 1) end
+local Methods = {}
+local mt      = {__index = Methods}
 
-Statsd.buffer = {} -- this table will be shared per worker process
-                   -- if lua_code_cache is off, it will be cleared every request
-
-Statsd.flush = function(sock, host, port)
-   if sock then -- send buffer
-      pcall(function()
-               local udp = sock()
-               udp:setpeername(host, port)
-               udp:send(Statsd.buffer)
-               udp:close()
-            end)
-   end
-   
-   -- empty buffer
-   for k in pairs(Statsd.buffer) do Statsd.buffer[k] = nil end
+function Methods:time(bucket, time)
+  self:register(bucket, time, "ms")
 end
 
-Statsd.register = function (bucket, suffix, sample_rate)
-   table.insert(Statsd.buffer, bucket .. ":" .. suffix .. "\n")
+function Methods:count(bucket, n)
+  self:register(bucket, n, 'c')
 end
 
-return Statsd 
+function Methods:incr(bucket, n)
+  self:count(bucket, 1)
+end
+
+function Methods:register(bucket, amount, suffix)
+  local dict = ngx.shared[self.dict]
+
+  dict:add('last_id', 0)
+  local last_id = assert(dict:incr('last_id', 1))
+
+  assert(dict:set(last_id, bucket .. ":" .. tostring(amount) .. "|" .. suffix .. "\n"))
+end
+
+function Methods:flush()
+  local dict = ngx.shared[self.dict]
+
+  local last_id         = tonumber(dict:get('last_id'), 10) or 0
+  local last_flushed_id = tonumber(dict:get('last_flushed_id'), 10) or 0
+
+  if last_id - last_flushed_id >= self.buffer_size then
+    assert(dict:set('last_flushed_id', last_id))
+    local buffer, len = {}, 0
+    for i=last_flushed_id+1, last_id do
+      len = len + 1
+      buffer[len] = assert(dict:get(i))
+      dict:delete(i)
+    end
+
+    local udp = ngx.socket.udp()
+    udp:setpeername(self.host, self.port)
+    udp:send(buffer)
+    udp:close()
+  end
+end
+
+statsd.new = function(host, port, dict, buffer_size)
+  return setmetatable({
+    host        = host or '127.0.0.1',
+    port        = port or 8125,
+    dict        = dict or 'STATSD',
+    buffer_size = buffer_size or 50
+  }, mt)
+end
+
+return statsd
--- spec/statsd_spec.lua
+++ spec/statsd_spec.lua
@@ -1,51 +1,82 @@
-require("busted")
-
-local statsd = require("lib.statsd")
+local fakengx = require 'spec.lib.fakengx'
+local statsd  = require 'lib.statsd'
 
 describe("statsd", function()
+  local s, dict
+  before_each(function()
+    ngx  = fakengx.new()
+    s    = statsd:new()
+    dict = ngx.shared[s.dict]
+  end)
+
   describe("count", function()
     it("registers a count metric for the bucket", function()
-      local s = spy.on(statsd, "register")
-      statsd.count("bucket", 99)
-      assert.spy(s).was.called_with("bucket", "99|c")
+      local sp = spy.on(s, "register")
+      s:count("bucket", 99)
+      assert.spy(sp).was.called_with(s, "bucket", 99, "c")
     end)
   end)
 
   describe("incr", function()
     it("makes a count of one for the bucket", function()
-      local s = spy.on(statsd, "count")
-      statsd.incr("bucket")
-      assert.spy(s).was.called_with("bucket", 1)
+      local sp = spy.on(s, "count")
+      s:incr("bucket")
+      assert.spy(sp).was.called_with(s, "bucket", 1)
     end)
   end)
 
   describe("time", function()
     it("registers a time metric for the bucket", function()
-      local s = spy.on(statsd, "register")
-      statsd.time("bucket", 100)
-      assert.spy(s).was.called_with("bucket", "100|ms")
+      local sp = spy.on(s, "register")
+      s:time("bucket", 100)
+      assert.spy(sp).was.called_with(s, "bucket", 100, 'ms')
     end)
   end)
 
   describe("register", function()
     it("appends a metric string terminated with a newline to the buffer", function()
-      statsd.flush()
-      statsd.register("foo", "bar")
-      statsd.register("zip", "zap")
-      assert.are.equal(statsd.buffer[1], "foo:bar\n")
-      assert.are.equal(statsd.buffer[2], "zip:zap\n")
-    end)
+      assert.is_nil(dict:get('last_id'))
 
+      s:register("foo", "bar", "baz")
+      assert.are.equal(dict:get(1), "foo:bar|baz\n")
+      assert.are.equal(dict:get('last_id'), 1)
+
+      s:register("quux", "corge", "grault")
+      assert.are.equal(dict:get(2), "quux:corge|grault\n")
+      assert.are.equal(dict:get('last_id'), 2)
+    end)
   end)
 
   describe("flush", function()
-    it("empties the buffer", function()
-      statsd.buffer[1] = "something"
-      statsd.flush()
-      assert.are.equal(table.getn(statsd.buffer), 0)
+    it("does nothing if the buffer is not filled up", function()
+      s:register("foo", "bar", "baz")
+      s:flush()
+      assert.is_nil(dict:get('last_flushed_id'))
+      assert.are.equal(ngx.shared.STATSD:get(1), "foo:bar|baz\n")
     end)
 
-    pending("sends the buffer to statsd via UDP to specified host and port", function()
+    it("#focus sends the buffer to statsd via UDP to specified host and port", function()
+      local mocksocket = {setpeername = function() end, close = function() end}
+      local sent = nil
+      mocksocket.send = function(_, x) sent = x end
+      ngx.socket.udp = function() return mocksocket end
+
+      local setpeername = spy.on(mocksocket, 'setpeername')
+      local close       = spy.on(mocksocket, 'close')
+
+      local s = statsd.new(nil, nil, nil, 1)
+
+      s:register("foo", "bar", "baz")
+      s:register("quux", "corge", "grault")
+      s:flush()
+
+      assert.spy(setpeername).was.called_with(mocksocket, s.host, s.port)
+      assert.spy(close).was.called_with(mocksocket)
+
+      assert.are.same(sent, {'foo:bar|baz\n', 'quux:corge|grault\n'})
+
+      assert.are.equal(dict:get('last_id'), 2)
+      assert.are.equal(dict:get('last_flushed_id'), 2)
     end)
   end)
 
